18be393874a91d01f1db720557d049a59dcee63c
deeksha-aashri <deekshaaashri1@gmail.com>
2022-12-16 13:57:08 +0530

    :memo: socket.io started

---
+++ index.js
@@ -0,0 +1,22 @@
+const io=require('socket.io')(3000);//any port can be taken. Socket.io server is attached to an instance of http. It listens to incoming events.
+//socket is a particular connection
+
+const users={};
+
+//io.on v/s socket.on both taken in custom events and callbacks
+//io.on is actually socket.io.on that is, it is a socket.io instance of http. It listens to multiple socket connections
+//socket.on works on a particular connection
+io.on('connection', socket=>{
+  
+    socket.on('new-user-joined', name=>{
+     users[socket.id]=name;
+     socket.broadcast.emit('user-joined', name)//socket.broadcast.emit emits a msg to all except the socket connection which has joined 
+  });
+
+socket.on('send', message=>{
+    socket.broadcast.emit('receive', {message:message, name:users[socket.id]})
+});
+
+
+
+})
